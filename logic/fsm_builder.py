fe715fcde1355ceb05e74b78281057fe1d2cc5a9
AxodenelCorvus <Zues01222@gmail.com>
2025-11-07 07:36:52 -0500

    Convert lambda symbol to empty str in FSMBuilder

--- logic/fsm_builder.py
+++ logic/fsm_builder.py
@@ -4,19 +4,21 @@ from graph_items.edge_item import EdgeItem
 from graph_items.node_item import NodeItem
 
 class FSMBuilder(MachineModel):
-    # Self will now contain a MachineModel object within it (type of inheritence)
+
     def build(self):
-        # Get the transitions and edges to create a dictionary in the form
-        # 'start state' : {'symbol': 'end state', 'symbol': 'end state'}
-        # Basically you're building the transitions dictionary
-        
         nfa_transitions = {}
         for node in self.nodes:
             curr_state_transitions = {}
             for edge in node.edges:
-                if edge.symbol not in curr_state_transitions:
-                    curr_state_transitions[edge.symbol] = set()
-                curr_state_transitions[edge.symbol].add(edge.node2.name)
+                edge_sym = edge.symbol
+
+                #Convert lambda to empty string for NFA machine to handle
+                if edge_sym == "λ":
+                    edge_sym = ''
+
+                if edge_sym not in curr_state_transitions:
+                    curr_state_transitions[edge_sym] = set()
+                curr_state_transitions[edge_sym].add(edge.node2.name)
             nfa_transitions[node.name] = curr_state_transitions
         
         nfa = NFA(
